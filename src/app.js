49a9e15d878a4cfa8cc74f145f0f6378a1ac29c0
FabMP <fabricio05mp@gmail.com>
2024-07-04 12:43:17 -0300

    Settings fixed

--- src/app.js
+++ src/app.js
@@ -21,6 +21,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', rotaIndex);
 
+const url = `https://royalroad.onrender.com/`; // Replace with your Render URL
+const interval = 30000; // Interval in milliseconds (30 seconds)
+
+//Reloader Function
+function reloadWebsite() {
+  axios.get(url)
+    .then(response => {
+      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+    })
+    .catch(error => {
+      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+    });
+}
+
+setInterval(reloadWebsite, interval);
+
 app.listen(PORT, () => {
     console.log(`Iniciando servidor no ambiente ${process.env.NODE_ENV}, porta: ${PORT}`);
 })
