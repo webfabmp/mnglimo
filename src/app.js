48cbacccf0ecc37850766e181eebc484d2e11a19
FabMP <fabricio05mp@gmail.com>
2024-07-04 12:47:50 -0300

    Settings fixed

--- src/app.js
+++ src/app.js
@@ -26,14 +26,21 @@ const interval = 30000; // Interval in milliseconds (30 seconds)
 
 //Reloader Function
 function reloadWebsite() {
-  axios.get(url)
-    .then(response => {
-      console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
-    })
-    .catch(error => {
-      console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
-    });
-}
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(`Reloaded at ${new Date().toISOString()}: Status Code ${response.status}`);
+        // Faça algo com os dados recebidos (se necessário)
+      })
+      .catch(error => {
+        console.error(`Error reloading at ${new Date().toISOString()}:`, error.message);
+      });
+  }
 
 setInterval(reloadWebsite, interval);
 
