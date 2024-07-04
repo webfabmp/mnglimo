adf87aac5e716f718ff8a7593d9318f282350b54
FabMP <fabricio05mp@gmail.com>
2024-07-03 22:22:36 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### Web app for Royal Road Limo Services
--- src/app.js
+++ src/app.js
@@ -1,25 +1,25 @@
 var express = require('express');
 const rotaIndex = require('./rotas/index')
-const rotaPostagem = require('./rotas/postagens'); 
+const expressEjsLayouts = require('express-ejs-layouts');
+const path = require('path')
 
 var app = express(); 
-var expressLayouts = require('express-ejs-layouts');
 
 app.use(express.json());
+
+app.use(expressEjsLayouts)
 
 const PORT = 3000;
 
 app.set('view engine', 'ejs');
 
-app.use(expressLayouts);
-
 app.set('layout', 'layouts/layoutblog'); 
 
 app.use('/static', express.static('public'));
 
-app.use('/', rotaIndex);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/postagens', rotaPostagem);
+app.use('/', rotaIndex);
 
 app.listen(PORT, () => {
     console.log(`Iniciando servidor no ambiente ${process.env.NODE_ENV}, porta: ${PORT}`);
