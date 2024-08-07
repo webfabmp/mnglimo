87ecc0ff5999e06f4add35f88e40a6150bc40e03
FabMP <fabricio05mp@gmail.com>
2024-08-07 02:29:52 -0300

    Sitemap updated

--- src/app.js
+++ src/app.js
@@ -44,6 +44,34 @@ function reloadWebsite() {
 
 setInterval(reloadWebsite, interval);
 
+app.use('/sitemap.xml', async (req, res, next) => {
+  res.type('text/xml')
+  res.send(
+    `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset
+          xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <!-- created with Free Online Sitemap Generator www.xml-sitemaps.com -->
+    
+    
+    <url>
+      <loc>https://www.mnglimollc.com/</loc>
+      <lastmod>2024-08-05T05:51:57+00:00</lastmod>
+      <priority>1.00</priority>
+    </url>
+    <url>
+      <loc>https://www.mnglimollc.com/contact</loc>
+      <lastmod>2024-08-05T05:51:57+00:00</lastmod>
+      <priority>0.80</priority>
+    </url>
+    
+    
+    </urlset>`
+  )
+});
+
 app.listen(PORT, () => {
     console.log(`Iniciando servidor no ambiente ${process.env.NODE_ENV}, porta: ${PORT}`);
 })
